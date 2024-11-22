6473a81e1f4772664a96f430f2cc7937a5c22115
Aanchal Rana <aanchalrana0106@gmail.com>
2024-11-22 19:05:14 +0530

    Update converter.py

--- face_recognition/converter.py
+++ face_recognition/converter.py
@@ -1,7 +1,8 @@
 from PIL import Image
+#use this file if the image is not in RGB or formatted according to the proogram
 
 # Open the image and convert it to RGB format
-img = Image.open("kareenaa.jpg").convert("RGB")
-img.save("kareena.jpg")  # Save the converted image
+img = Image.open("Ruchika.jpg").convert("RGB")
+img.save("Ruchika.jpg")  # Save the converted image
 
 
